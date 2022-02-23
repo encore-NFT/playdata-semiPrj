702d4d92ab0332b8840de14d3889e70808e37530
carl020958 <carl020958@korea.ac.kr>
2022-02-23 19:10:24 +0900

    deleted ./DS_Store files amended docker-compose.yml & build.sh due to unchanged codes needed to be fixed

--- spark/docker_images/hadoop/build.sh
+++ spark/docker_images/hadoop/build.sh
@@ -4,52 +4,40 @@
 # intel
 #--------
 # hadoop-base
-docker build --no-cache \
-  -t hadoop-base:hadoop3.2.2-java8 ./base
+docker build --no-cache -t hadoop-base:hadoop3.2.2-java8 ./base
 
 # hadoop-datanode
-docker build --no-cache \
-  -t hadoop-datanode:hadoop3.2.2-java8 ./datanode
+docker build --no-cache -t hadoop-datanode:hadoop3.2.2-java8 ./datanode
 
 # hadoop-historyserver
-docker build --no-cache \ 
-  -t hadoop-historyserver:hadoop3.2.2-java8 ./historyserver
+docker build --no-cache -t hadoop-historyserver:hadoop3.2.2-java8 ./historyserver
 
 # hadoop-namenode
-docker build --no-cache \
-  -t carl020958/hadoop-namenode:hadoop3.2.2-java8 ./namenode
+docker build --no-cache -t hadoop-namenode:hadoop3.2.2-java8 ./namenode
 
 # hadoop-nodemanager
-docker build --no-cache \
-  -t carl020958/hadoop-nodemanager:hadoop3.2.2-java8 ./nodemanager
+docker build --no-cache -t hadoop-nodemanager:hadoop3.2.2-java8 ./nodemanager
 
 # # hadoop-resourcemanager
-docker build --no-cache \
-  -t carl020958/hadoop-resourcemanager:hadoop3.2.2-java8 ./resourcemanager
+docker build --no-cache -t hadoop-resourcemanager:hadoop3.2.2-java8 ./resourcemanager
 
 #--------
 # m1
 #--------
 # # hadoop-base
-# docker build --platform linux/amd64 --no-cache \
-#   -t hadoop-base:hadoop3.2.2-java8 ./base
+# docker build --platform linux/amd64 --no-cache -t hadoop-base:hadoop3.2.2-java8 ./base
 
 # # hadoop-datanode
-# docker build --platform linux/amd64 --no-cache \
-#   -t hadoop-datanode:hadoop3.2.2-java8 ./datanode
+# docker build --platform linux/amd64 --no-cache -t hadoop-datanode:hadoop3.2.2-java8 ./datanode
 
 # # hadoop-historyserver
-# docker build --platform linux/amd64 --no-cache \ 
-#   -t hadoop-historyserver:hadoop3.2.2-java8 ./historyserver
+# docker build --platform linux/amd64 --no-cache -t hadoop-historyserver:hadoop3.2.2-java8 ./historyserver
 
 # # hadoop-namenode
-# docker build --platform linux/amd64 --no-cache \
-#   -t carl020958/hadoop-namenode:hadoop3.2.2-java8 ./namenode
+# docker build --platform linux/amd64 --no-cache -t hadoop-namenode:hadoop3.2.2-java8 ./namenode
 
 # # hadoop-nodemanager
-# docker build --platform linux/amd64 --no-cache \
-#   -t carl020958/hadoop-nodemanager:hadoop3.2.2-java8 ./nodemanager
+# docker build --platform linux/amd64 --no-cache -t hadoop-nodemanager:hadoop3.2.2-java8 ./nodemanager
 
 # # # hadoop-resourcemanager
-# docker build --platform linux/amd64 --no-cache \
-#   -t carl020958/hadoop-resourcemanager:hadoop3.2.2-java8 ./resourcemanager
+# docker build --platform linux/amd64 --no-cache -t hadoop-resourcemanager:hadoop3.2.2-java8 ./resourcemanager
